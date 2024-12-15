039719377882e9d177cc3fa4a214580e3ab7003c
Ayo Giwa <ayogivva.gmail.com>
2024-12-15 23:15:32 +0000

    .

--- manager/urls.py
+++ manager/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('manager_webapp.urls')),
+    path('/', include('manager_webapp.urls')),
 ]
