3270b90a973f09564d5775f66600b698c8ffe2b3
Ayo Giwa <ayogivva.gmail.com>
2024-12-15 17:41:18 +0000

    Production setup

---
+++ manager_webapp/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from . import views
+urlpatterns = [
+    path('', views.home, name=""),
+
+#     path('register', views.register, name="register"),
+    
+#     path('login', views.login, name="login"),
+
+#     path('logout', views.logout, name="logout"),
+
+#     path('dashboard', views.dashboard, name="dashboard"),
+
+#     path('admin-dashboard', views.adminDashboard, name="admin-dashboard"),
+
+#     path('admin-updates/<int:pk>', views.adminUpdateRequest, name='admin-updates'),
+
+#     path('create-record', views.createRecord, name="create-record"),
+
+#     path('update-record/<int:pk>/', views.updateRecord, name="update-record"),
+    
+#     path('request/<int:pk>', views.viewRecord, name="view-request"),
+
+#     path('delete-record/<int:pk>', views.deleteRecord, name="delete-record"),
+ ]
