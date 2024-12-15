fcbd56e73fd84003c52f9862cff38a040e3ce95a
Ayo Giwa <ayogivva.gmail.com>
2024-12-15 22:07:14 +0000

    Set homepage for deploy

--- manager_webapp/urls.py
+++ manager_webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('', views.home, name=""),
+    path('', views.home, name="home"),
 
 #     path('register', views.register, name="register"),
     
